29a2cbff2ac9d16dfc7d260e615b05bafd1bba85
Marcos Vinicius Nunes <53801645+imarcosvinicius@users.noreply.github.com>
2021-12-14 12:55:00 -0300

    Concluindo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +22,41 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJim = new Dev();
+        devJim.setNome("Jim Carrey");
+        devJim.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Jim Carrey: " + devJim.getConteudosIncritos());
+        devJim.progredir();
+        devJim.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Jim Carrey: " + devJim.getConteudosIncritos());
+        System.out.println("Conteúdos Concluídos Jim Carrey: " + devJim.getConteudosConcluidos());
+        System.out.println("XP: " + devJim.calcularXp());
+
+        System.out.println("-----------");
+
+        Dev devMichael = new Dev();
+        devMichael.setNome("Michael Jackson");
+        devMichael.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Michael Jackson: " + devMichael.getConteudosIncritos());
+        devMichael.progredir();
+        devMichael.progredir();
+        devMichael.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Michael Jackson: " + devMichael.getConteudosIncritos());
+        System.out.println("Conteúdos Concluídos Michael Jackson: " + devMichael.getConteudosConcluidos());
+        System.out.println("XP: " + devMichael.calcularXp());
+
 
 
     }
